8b1cb7a9c4b9715a7be6ba98608536a6c30dbf81
SanFiltre <loic.blanc@hotmail.fr>
2015-11-04 08:34:10 +0100

    Création de la solution IHM-Serveur

---
+++ IHM-Serveur/KukaAgylus/KukaAgylus/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KukaAgylus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult BtnConnect_Click(object sender, EventArgs e)
+        {
+            ((List<Models.Log>)Session["LOGS"]).Add(new Models.Log("infos", "Connecting to the device ..."));
+            // ADD CONNECTION HERE
+
+            return View("Index");
+        }
+    }
+}
---
+++ IHM-Serveur/KukaAgylus/KukaAgylus/Models/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KukaAgylus.Models
+{
+    public class Log
+    {
+        public string Message { get; private set; }
+
+        public string LogType { get; private set; }
+
+        public DateTime Time { get; private set; }
+
+        public Log(string logType, string message)
+        {
+            this.Message = message;
+            this.LogType = logType;
+            this.Time = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}] {1}: {2}", LogType.ToUpper(), Time.ToLongTimeString(), Message);
+        }
+
+    }
+}
