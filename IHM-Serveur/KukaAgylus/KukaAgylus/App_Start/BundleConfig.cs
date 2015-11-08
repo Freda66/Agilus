f9eb0b5ef1e55547ed4edfd1cbab6c5350300b0f
JusteBlanc <loic.blanc@hotmail.fr>
2015-11-09 00:55:20 +0100

    Ajout de la modal de calibration du plateau Ajout de style Ajout d'images Ajout de la lib jquery-ui-1.11.3.js

--- IHM-Serveur/KukaAgylus/KukaAgylus/App_Start/BundleConfig.cs
+++ IHM-Serveur/KukaAgylus/KukaAgylus/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace KukaAgylus
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,6 +26,7 @@ namespace KukaAgylus
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/jquery-ui.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
